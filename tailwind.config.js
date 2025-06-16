e958a8519bf1a725e54a3d02c8e1770cd29c7a1e
ccristina24 <cristina@codemaker-s.com>
2025-06-16 20:27:18 +0200

    added colors to tailwind from our brand style guide

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          orange: "#FF824E",
+          purple: "#8588B8",
+          blue: "#AEE1ED",
+        },
+        dark_primary: {
+          orange: "#D75722",
+          purple: "#5A5C7F",
+          blue: "#D75722",
+        },
+      },
+    },
+  },
+};
